e59576ec4b2477fe7d58664e0caa266c9492e791
Bruno Pena <brunoph26@gmail.com>
2022-02-02 14:39:35 -0500

    Se arregló el schema

--- db/schema.rb
+++ db/schema.rb
@@ -47,6 +47,13 @@ ActiveRecord::Schema.define(version: 2022_02_01_221034) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.text "bio", null: false
+    t.text "address", null: false
+    t.integer "phone_number", null: false
+    t.integer "rating", null: false
+    t.boolean "chef", default: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
