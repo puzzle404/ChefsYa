895fe26d7b78c2096a8d9a0d8758cd2e32bb4535
Bruno Pena <brunoph26@gmail.com>
2022-02-03 11:58:55 -0500

    Se realizaron cambios en routes.rb

--- db/schema.rb
+++ db/schema.rb
@@ -41,7 +41,6 @@ ActiveRecord::Schema.define(version: 2022_02_02_213518) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "chef_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
