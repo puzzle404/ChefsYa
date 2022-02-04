734ae3e7249839f0f3e477735e8cc039bb61f8f2
Manuel Ferrer <cmanuferrer@gmail.com>
2022-02-04 13:38:50 -0300

    push de dishes view

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ Dish.destroy_all
 User.destroy_all
 p "creando seeds"
 chef1 = User.create!(email: "cmanuferre2@gmail.com", first_name: "manuel", last_name: "ferrer",
-  bio: "asldlkañlsdkñlaskdñlaskdñlaskdlñaskdñlaskdñlaskdañlsdkañlsdkasñldkañsldkañsldkaslñdkasñldkasñldkaslñdkaslñdkaslñdkaslñdkaslñdkalñsdkasñldkaslñdkaslñdkaslñd" , address: "lopez 565", phone_number: 12312312, chef: true, password: "123456")
+  bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat distinctio ad obcaecati eaque voluptatibus adipisci nostrum laudantium sit dolor. Sunt praesentium cumque reiciendis assumenda aperiam minima laudantium laborum saepe! Exercitationem." , address: "lopez 565", phone_number: 12312312, chef: true, password: "123456")
 dish2 = Dish.create!(title: "pollo", description: "muy rico", price: 123123, category: "peruana", chef: chef1)
 dish2 = Dish.create!(title: "pollo", description: "muy rico", price: 123123, category: "peruana", chef: chef1)
 dish3 = Dish.create!(title: "pollo", description: "muy rico", price: 123123, category: "peruana", chef: chef1)
