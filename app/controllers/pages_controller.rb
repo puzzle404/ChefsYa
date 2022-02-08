77b4a725fd1f9d6444a2402b58d5d4f07abed277
Bruno Pena <brunoph26@gmail.com>
2022-02-07 20:37:14 -0500

    Se agregó filtro por barra de búsqueda

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,16 +3,23 @@ class PagesController < ApplicationController
 
   def home
     @chefs = User.where(chef: true).last(3).reverse
-
   end
 
   def chefs
-    @chefs = User.where(chef: true)
-    @dishes = Dish.all
-    # @categories = Dish.select(:category).distinct.where(chef_id: @chef)
+    if params[:query].present?
+      sql_query = "title ILIKE :query OR description ILIKE :query"
+      @dishes = Dish.where(sql_query, query: "%#{params[:query]}%")
 
-    # @categories = Dish.select(:category).distinct.where(chef_id: @chef)
+      @dishes.each do |dish|
+        @chefs = User.where(id: dish.chef_id)
+      end
+    else
+        @chefs = User.where(chef: true)
+        @dishes = Dish.all
+    end
   end
+    # @categories = Dish.select(:category).distinct.where(chef_id: @chef)
+    # @categories = Dish.select(:category).distinct.where(chef_id: @chef)
 
   def chefs_dashboard
     @chefs = User.where(chef: true)
