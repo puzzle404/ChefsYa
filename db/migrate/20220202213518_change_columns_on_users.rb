92d3288a0a1f486334477f5aebcc86374043d2e1
Manuel Ferrer <cmanuferrer@gmail.com>
2022-02-10 17:57:14 -0300

    Heroku deploy

--- db/migrate/20220202213518_change_columns_on_users.rb
+++ db/migrate/20220202213518_change_columns_on_users.rb
@@ -1,6 +1,5 @@
 class ChangeColumnsOnUsers < ActiveRecord::Migration[6.1]
   def change
-    change_column :users, :phone_number, :integer, null: true
     change_column :users, :address, :text, null: true
   end
 end
